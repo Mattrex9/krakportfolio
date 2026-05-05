bbb3a2d7bd8b24c6f425af00017b8477fb5377d9
Matthew Krakowiak <matthewkrakowiak@MacBook-Air-1394.local>
2026-05-05 17:25:13 -0400

    updated portfolio

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Matthew Krakowiak - Portfolio",
+  title: "Matthew Krakowiak",
   description: "Generated Matthew Krakowiak's portfolio using Next.js and Tailwind CSS",
 };
 
